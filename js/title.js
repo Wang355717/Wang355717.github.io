b45bf217e11dfa5306ee850ddc20bbce8788220d
Wang355717 <3557173481@qq.com>
2023-05-21 12:28:22 +0800

    Site updated: 2023-05-21 12:28:19

--- js/title.js
+++ js/title.js
@@ -4,7 +4,7 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '跑哪里去了~';
+    document.title = '跑到哪里去了？主人';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
